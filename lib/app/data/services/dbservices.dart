393306d047aedf9645ca95354d2103c8458a1c43
Kenny Chou <kennyc015@gmail.com>
2022-10-03 21:29:15 +0800

    add multi seleict

--- lib/app/data/services/dbservices.dart
+++ lib/app/data/services/dbservices.dart
@@ -1,9 +1,13 @@
+import 'package:flutter/foundation.dart';
 import 'package:get/get.dart';
-import 'package:hive_flutter/hive_flutter.dart';
 
 import 'package:flutter_froghome_app/app/data/models/froghome_model.dart';
 import 'package:flutter_froghome_app/app/data/provider/plot_provider.dart';
 import 'package:flutter_froghome_app/app/data/provider/base_provider.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+// import 'package:hive_flutter/hive_flutter.dart';
+import 'package:path_provider/path_provider.dart';
 
 class DBService extends GetxService {
   static final _base_provider = BaseProvider();
@@ -14,7 +18,16 @@ class DBService extends GetxService {
 
   Future<DBService> init() async {
     print('_________DBService init___________');
-    await Hive.initFlutter();
+
+    if (kIsWeb) {
+      Hive.initFlutter();
+    } else {
+      final appDocumentDir = await getApplicationSupportDirectory();
+      // Hive.init(appDocumentDir.path);
+      // await Hive.init(null);
+      print(appDocumentDir.path);
+      Hive.init(appDocumentDir.path);
+    }
     Hive.registerAdapter(PlotAdapter());
     _base_provider.init();
     await _plot_provider.init();
