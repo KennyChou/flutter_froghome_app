9b0d3c58bf8b0dccd07168a706abd218c3c56eca
Kenny Chou <kennyc015@gmail.com>
2022-10-07 09:35:13 +0000

    change theme

--- lib/main.dart
+++ lib/main.dart
@@ -12,33 +12,51 @@ void main() {
       title: "Application",
       initialRoute: AppPages.INITIAL,
       getPages: AppPages.routes,
+      // theme: FlexThemeData.light(
+      //   scheme: FlexScheme.dellGenoa,
+      //   surfaceMode: FlexSurfaceMode.highScaffoldLowSurface,
+      //   blendLevel: 20,
+      //   appBarStyle: FlexAppBarStyle.primary,
+      //   appBarOpacity: 0.95,
+      //   subThemesData: const FlexSubThemesData(
+      //     blendOnLevel: 20,
+      //     blendOnColors: false,
+      //   ),
+      //   visualDensity: FlexColorScheme.comfortablePlatformDensity,
+      // ),
+      // darkTheme: FlexThemeData.dark(
+      //   colors: FlexColor.schemes[FlexScheme.dellGenoa]!.light.defaultError
+      //       .toDark(10, true),
+      //   surfaceMode: FlexSurfaceMode.highScaffoldLowSurface,
+      //   blendLevel: 15,
+      //   appBarStyle: FlexAppBarStyle.primary,
+      //   appBarOpacity: 0.90,
+      //   subThemesData: const FlexSubThemesData(
+      //     blendOnLevel: 30,
+      //   ),
+      //   visualDensity: FlexColorScheme.comfortablePlatformDensity,
+      //   // To use the playground font, add GoogleFonts package and uncomment
+      //   // fontFamily: GoogleFonts.notoSans().fontFamily,
+      // ),
       theme: FlexThemeData.light(
-        scheme: FlexScheme.dellGenoa,
+        scheme: FlexScheme.green,
         surfaceMode: FlexSurfaceMode.highScaffoldLowSurface,
         blendLevel: 20,
-        appBarStyle: FlexAppBarStyle.primary,
         appBarOpacity: 0.95,
-        subThemesData: const FlexSubThemesData(
-          blendOnLevel: 20,
-          blendOnColors: false,
-        ),
-        visualDensity: FlexColorScheme.comfortablePlatformDensity,
-      ),
-      darkTheme: FlexThemeData.dark(
-        colors: FlexColor.schemes[FlexScheme.dellGenoa]!.light.defaultError
-            .toDark(10, true),
-        surfaceMode: FlexSurfaceMode.highScaffoldLowSurface,
-        blendLevel: 15,
-        appBarStyle: FlexAppBarStyle.primary,
-        appBarOpacity: 0.90,
-        subThemesData: const FlexSubThemesData(
-          blendOnLevel: 30,
-        ),
         visualDensity: FlexColorScheme.comfortablePlatformDensity,
         // To use the playground font, add GoogleFonts package and uncomment
         // fontFamily: GoogleFonts.notoSans().fontFamily,
       ),
-      themeMode: ThemeMode.light,
+      darkTheme: FlexThemeData.dark(
+        scheme: FlexScheme.green,
+        surfaceMode: FlexSurfaceMode.highScaffoldLowSurface,
+        blendLevel: 15,
+        appBarOpacity: 0.90,
+        visualDensity: FlexColorScheme.comfortablePlatformDensity,
+        // To use the playground font, add GoogleFonts package and uncomment
+        // fontFamily: GoogleFonts.notoSans().fontFamily,
+      ),
+      themeMode: ThemeMode.dark,
       localizationsDelegates: [
         GlobalMaterialLocalizations.delegate,
         GlobalWidgetsLocalizations.delegate,
