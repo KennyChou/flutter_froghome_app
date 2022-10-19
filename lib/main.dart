f2f147f6df7be6abd6f20e7b0b56088020730972
Kenny Chou <kennyc015@gmail.com>
2022-10-19 22:28:30 +0800

    fixed firebase in windows

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_froghome_app/app/theme/custum_theme.dart';
 import 'package:flutter_froghome_app/firebase_options.dart';
@@ -10,9 +13,12 @@ import 'app/routes/app_pages.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   // SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  if (Platform.isAndroid || kIsWeb) {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+  }
+
   runApp(
     GetMaterialApp.router(
       title: "蛙記錄",
